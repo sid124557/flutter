305c8ee0837439e733017071ca7336624063dc4a
sid124557 <sid124557@gmail.com>
2021-02-28 02:01:44 +0530

    state full wigite implimentation

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,37 @@ import 'package:flutter/material.dart';
 
 void main(){
     runApp( Myapp());
-    var Questions=["what is your faverate colour?","what is your faverate animal?"];
     }
 
-class Myapp extends StatelessWidget {
+class Myapp extends StatefulWidget {
   @override
+  State<StatefulWidget> createState(){
+    return MyappState();
+  }
+}
+class MyappState extends State<Myapp> { 
+  var questionIndex=0;
+  void answerQuestion(){
+    setState(()=>{
+          questionIndex=questionIndex+1
+    });
+    
+    print(questionIndex);
+    print("quesion answered");
+  }
+  
+  @override
+  // ignore: override_on_non_overriding_field
+  var Questions =["what is your faverate colour?","what is your faverate animal?"];
   Widget build(BuildContext context) {
     return MaterialApp(home: Scaffold( 
       appBar: AppBar(title:Text('My first app'),),
       body: Column
       (children:[
-        Text('This is the Question'),
-        RaisedButton(child: Text('Answer Q1'),onPressed: null),
-        RaisedButton(child: Text('Answer Q2'),onPressed: null),
-        RaisedButton(child: Text('Answer Q3'),onPressed: null),
+        Text(Questions[questionIndex]),
+        RaisedButton(child: Text('Answer Q1'),onPressed: answerQuestion),
+        RaisedButton(child: Text('Answer Q2'),onPressed: answerQuestion),
+        RaisedButton(child: Text('Answer Q3'),onPressed: answerQuestion),
       ],) ,
     ),);
   }
