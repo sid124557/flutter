f2d7e9d074cbc74f2b4c09507b7352aac5a5f693
sid124557 <sid124557@gmail.com>
2021-02-28 00:29:03 +0530

    quiz app

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp( Myapp());
-}
+    runApp( Myapp());
+    var Questions=["what is your faverate colour?","what is your faverate animal?"];
+    }
 
 class Myapp extends StatelessWidget {
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('hello siddharth'),);
+    return MaterialApp(home: Scaffold( 
+      appBar: AppBar(title:Text('My first app'),),
+      body: Column
+      (children:[
+        Text('This is the Question'),
+        RaisedButton(child: Text('Answer Q1'),onPressed: null),
+        RaisedButton(child: Text('Answer Q2'),onPressed: null),
+        RaisedButton(child: Text('Answer Q3'),onPressed: null),
+      ],) ,
+    ),);
   }
 }
