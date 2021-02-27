5f69327f3daf69aa7e6b120a3b95b7195fb79eed
sid124557 <sid124557@gmail.com>
2021-02-27 22:58:56 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp( Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('hello siddharth'),);
+  }
+}
